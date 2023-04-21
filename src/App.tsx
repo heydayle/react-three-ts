fe6e1e7ef6fa266b340086d9a08685b7ae3f9811
heyday <hungthinh.ckc@gmail.com>
2023-04-21 19:50:05 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ExampleThree from "./components/exampleThree";
+
+function App() {
+
+  return (
+    <div className="App">
+      <ExampleThree />
+    </div>
+  )
+}
+
+export default App
