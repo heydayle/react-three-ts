716bd7ad01fae6a7bbd5d105554193f96ed897c2
heyday <hungthinh.ckc@gmail.com>
2023-04-22 23:46:10 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ExampleThree from "./components/exampleThree";
+import { Leva } from 'leva'
 
 function App() {
 
   return (
     <div className="App">
       <ExampleThree />
+      <Leva collapsed />
     </div>
   )
 }
