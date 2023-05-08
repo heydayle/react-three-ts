21c6484235bca3c798cd5468536fbe59585ce99e
heyday <hungthinh.ckc@gmail.com>
2023-05-08 11:52:27 +0700

    Update around the Sun

--- src/components/center.tsx
+++ src/components/center.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import SphereObject from './blocks/sphereGeometry'
-import {Mesh, Vector2} from "three";
+import { Mesh } from "three";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 60 , window.innerWidth / window.innerHeight, 1, 1000 );
 
@@ -16,7 +16,7 @@ const planet = new THREE.Mesh( plan, materialPlan );
 planet.scale.set(0.1,0.1,0.1)
 planet.position.x = 150
 
-const pointLight = new THREE.PointLight(0xff842e, 0.9)
+const pointLight = new THREE.PointLight(0xff842e, 1)
 pointLight.position.set(0, 0, 0);
 pointLight.castShadow = true;
 
@@ -24,7 +24,7 @@ camera.position.set( 0, 20, 100 );
 scene.add( planet );
 planet.add( octahedron )
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.9)
 const Sun = new SphereObject()
 Sun.setMaterialColor('#ff842e', 1)
 Sun.setMeshSize(0.5,0.5,0.5)
@@ -56,7 +56,7 @@ const Mars = new SphereObject()
 Mars.setMaterialColor('#ab624a', 1)
 Mars.setMeshSize(0.15,0.15,0.15)
 Mars.mesh.position.x = -70
-Mars.mesh.position.z = -70
+Mars.mesh.position.y = -70
 Sun.mesh.add( Mars.mesh )
 
 const MoonEarth = new SphereObject()
@@ -90,12 +90,18 @@ const controller = () => {
 let sunPosition = Sun.mesh.position.clone();
 const aroundSun = (
     mesh: Mesh,
-    angle: number,
+    angleX: number,
+    angleY: number,
+    angleZ: number,
     reverse: Boolean = false,
     rotationOptions: { x: number, y: number, z: number } | undefined = undefined) => {
+    const x: number = reverse ? -1 : 1
     const y: number = reverse ? -1 : 1
+    const z: number = reverse ? -1 : 1
     mesh.position.sub(sunPosition)
-    mesh.position.applyAxisAngle(new THREE.Vector3(0, y, 0), angle);
+    mesh.position.applyAxisAngle(new THREE.Vector3(x, 0, 0), angleX);
+    mesh.position.applyAxisAngle(new THREE.Vector3(0, y, 0), angleY);
+    mesh.position.applyAxisAngle(new THREE.Vector3(0, 0, z), angleZ);
     mesh.position.add(sunPosition)
     if (rotationOptions) {
         mesh.rotation.x += rotationOptions.x
@@ -105,11 +111,11 @@ const aroundSun = (
 }
 function animate() {
     controller()
-    aroundSun(Mercury.mesh, 0.01, true)
-    aroundSun(Venus.mesh, 0.009, false, { x: 0.01, y: 0.01, z: 0.01})
-    aroundSun(Earth.mesh, 0.007, false, { x: 0, y: 0.01, z: 0.01})
-    aroundSun(Mars.mesh, 0.004, false, { x: 0.01, y: 0.01, z: 0.01})
-    aroundSun(planet, 0.001, false)
+    aroundSun(Mercury.mesh, 0, 0.01, 0, true)
+    aroundSun(Venus.mesh, 0, 0.009, 0, false, { x: 0.01, y: 0.01, z: 0.01})
+    aroundSun(Earth.mesh, 0, 0.007, 0, false, { x: 0, y: 0.01, z: 0.01})
+    aroundSun(Mars.mesh, 0.0001, 0.0001, 0.001, false, { x: 0.1, y: 1, z: 0.01})
+    aroundSun(planet, 0.001, 0.0009, 0, false)
     planet.rotation.y += 0.007
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
